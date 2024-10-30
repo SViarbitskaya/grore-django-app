053a830793bbb63a995221276973e2b2bc2f347a
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-30 07:15:30 +0100

    Service should work

--- setup-production.sh
+++ setup-production.sh
@@ -4,4 +4,7 @@ sudo mkdir -p ${DJANGO_STATIC_ROOT}
 sudo chown -R ${SYS_DJANGO_USER}:${SYS_DJANGO_GROUP} ${DJANGO_MEDIA_ROOT} ${DJANGO_STATIC_ROOT}
 sudo chmod -R 0775 ${DJANGO_MEDIA_ROOT} ${DJANGO_STATIC_ROOT}
 sudo chmod -R +s ${DJANGO_MEDIA_ROOT} ${DJANGO_STATIC_ROOT}
-sudo nnginx -t
+sduo ln -s `pwd`/grore.service /etc/systemd/system/grore.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now grore
+sudo nginx -t
