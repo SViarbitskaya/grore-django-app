40c3f02728bef37179a31f68673271b2ce893d70
Chris <christopher@mann.fr>
2024-11-06 15:37:53 +0100

    Update entrypoint.sh

--- scripts/docker/django/entrypoint.sh
+++ scripts/docker/django/entrypoint.sh
@@ -13,6 +13,4 @@ python -m venv $APP_CACHE_ROOT/.venv
 $APP_CACHE_ROOT/.venv/bin/pip install --upgrade pip
 $APP_CACHE_ROOT/.venv/bin/pip install -r $APP_DJANGO_ROOT/requirements.txt
 $APP_CACHE_ROOT/.venv/bin/python manage.py migrate
-$APP_CACHE_ROOT/.venv/bin/python manage.py collectstatic --noinput
 $APP_CACHE_ROOT/.venv/bin/python manage.py runserver ${DJANGO_HOST}:${DJANGO_PORT}
-
