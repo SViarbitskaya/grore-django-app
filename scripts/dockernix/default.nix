4e8b751c5cf0bb6c975ae0e7df87f2413ced55ce
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-08 16:46:46 +0100

    pip install correction

--- scripts/dockernix/default.nix
+++ scripts/dockernix/default.nix
@@ -22,7 +22,7 @@ pkgs.stdenv.mkDerivation {
     echo "python -m venv .venv" >> $out/bin/getgrore 
     echo "source .venv/bin/activate" >> $out/bin/getgrore 
     echo "cd $name" >> $out/bin/getgrore 
-    echo "pip install -r requirements" >> $out/bin/getgrore 
+    echo "../.venv/bin/pip install -r requirements.txt" >> $out/bin/getgrore 
     echo "cp scripts/sample.env .env" >> $out/bin/getgrore 
     chmod +x $out/bin/getgrore
   '';
