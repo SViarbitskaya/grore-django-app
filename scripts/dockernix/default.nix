09ad6842b16d878597ed73b3a5f9a66adcf51a6b
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-08 16:38:42 +0100

    imporved getgrore

--- scripts/dockernix/default.nix
+++ scripts/dockernix/default.nix
@@ -19,12 +19,17 @@ pkgs.stdenv.mkDerivation {
     cp $src $out/$pname.tar.gz
     echo "#!/bin/bash" > $out/bin/getgrore 
     echo "tar -xzf $out/$pname.tar.gz" >> $out/bin/getgrore 
+    echo "python -m venv .venv" >> $out/bin/getgrore 
+    echo "source .venv/bin/activate" >> $out/bin/getgrore 
+    echo "cd $name" >> $out/bin/getgrore 
+    echo "pip install -r requirements" >> $out/bin/getgrore 
+    echo "cp scripts/sample.env .env" >> $out/bin/getgrore 
     chmod +x $out/bin/getgrore
   '';
 
   postFixup = ''
     wrapProgram $out/bin/getgrore \
-      --set PATH ${pkgs.lib.makeBinPath [ pkgs.gnutar pkgs.gzip ]}
+      --set PATH ${pkgs.lib.makeBinPath [ pkgs.gnutar pkgs.gzip (import ./includes/pypackages.nix {inherit pkgs;}) ]}
   '';
 
   meta = with pkgs.lib; {
--- scripts/dockernix/includes/pypackages.nix
+++ scripts/dockernix/includes/pypackages.nix
@@ -1,18 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 # Python
 (pkgs.python311.withPackages (pyPkgs: [
-  pyPkgs.pillow
-  pyPkgs.pylibjpeg-libjpeg
-  pyPkgs.pypdf2
-  pyPkgs.python-ldap
-  pyPkgs.pq
   pyPkgs.aiosasl
-  pyPkgs.psycopg2
   pyPkgs.bleach
   pyPkgs.cffi
   pyPkgs.chardet
   pyPkgs.django
+  pyPkgs.django-appconf
+  pyPkgs.django-compressor
+  pyPkgs.django-debug-toolbar
   pyPkgs.django-formtools
+  pyPkgs.django-modeltranslation
   pyPkgs.django-picklefield
   pyPkgs.django-simple-captcha
   pyPkgs.django-statici18n
@@ -21,26 +19,33 @@
   pyPkgs.future
   pyPkgs.gunicorn
   pyPkgs.markdown
+  pyPkgs.numpy
   pyPkgs.openpyxl
+  pyPkgs.pandas
   pyPkgs.pillow
   pyPkgs.pip
+  pyPkgs.pq
+  pyPkgs.psycopg2
   pyPkgs.pycryptodome
   pyPkgs.pyjwt
-  pyPkgs.pysaml2
-  pyPkgs.python-dateutil
-  pyPkgs.python-ldap
-  pyPkgs.qrcode
-  pyPkgs.requests
-  pyPkgs.requests-oauthlib
-  pyPkgs.setuptools
-  pyPkgs.simplejson
-  pyPkgs.python3-gnutls
-  pyPkgs.pip
-  pyPkgs.venvShellHook
   pyPkgs.pylibjpeg-libjpeg
   pyPkgs.pypdf2
-  pyPkgs.pq
-  pyPkgs.aiosasl
-  pyPkgs.psycopg2
+  pyPkgs.pysaml2
+  pyPkgs.python-dateutil
   pyPkgs.python-dotenv
+  pyPkgs.python-ldap
+  pyPkgs.python3-gnutls
+  pyPkgs.pytz
+  pyPkgs.qrcode
+  pyPkgs.rcssmin
+  pyPkgs.requests
+  pyPkgs.requests-oauthlib
+  pyPkgs.rjsmin
+  pyPkgs.setuptools
+  pyPkgs.simplejson
+  pyPkgs.six
+  pyPkgs.sqlparse
+  pyPkgs.typing-extensions
+  pyPkgs.tzdata
+  pyPkgs.venvShellHook
 ]))
