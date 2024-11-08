3ce5b3156d5fbe5a0b055c49dd4f1f780ee838af
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-08 10:15:05 +0100

    Trying a nix-generation of docker images

---
+++ scripts/dockernix/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Creates docker image for Grore-Django-App";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.default = pkgs.callPackage ./includes/derviation.nix { };
+    }
+  );
+}
