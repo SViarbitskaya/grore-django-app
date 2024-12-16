b808d09f214ab7f45cb1e9898975e117c2869a53
Sviatlana Viarbitskaya <sviatlana.viarbitskaya@gmail.com>
2024-12-16 12:18:12 +0100

    adding translations

--- images/forms.py
+++ images/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.conf import settings
+from django.utils.translation import gettext
 
 class ImageSearchForm(forms.Form):
     search_query = forms.CharField(
         widget=forms.TextInput(attrs={
-            'placeholder': "Search",
+            'placeholder': gettext("Search"),
             'class': 'form-control me-2' 
         }),
         max_length=100,
--- images/urls.py
+++ images/urls.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views 
 from .views import download_images
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
     path("selection/", views.SelectionView.as_view(), name="selection"),  # Gallery view
     path("selection/delete/<int:image_id>/", views.SelectionView.as_view(), name="delete_image_from_selection"),  # Same view for delete
     path('download-images/', download_images, name='download_images'),
